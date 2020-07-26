5ee2a32816cd7feaab9433dd5a957b24978a2a0d
zhangkai <kai.zhang@veeder.com>
2020-07-26 12:06:07 +0800

    完善tcp server框架。增加关闭所有连接接口

--- tcpServer/Server.go
+++ tcpServer/Server.go
@@ -1,8 +1,10 @@
 package tcpServer
 
 import (
+	"context"
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Callback func(conn net.Conn, ch chan []byte)
@@ -11,17 +13,23 @@ type ServerInterface struct {
 	bufSize  int
 	ch       chan []byte
 	progress Callback
+	listen   net.Listener
+	ctx      context.Context
+	cancel   context.CancelFunc
+	wg       sync.WaitGroup
 }
 
-func (self *ServerInterface) Start(port string, pCallback Callback) error {
-	listen, err := net.Listen("tcp", "127.0.0.1:"+port)
+func (self *ServerInterface) Start(port string, pCallback Callback) (err error) {
+	self.listen, err = net.Listen("tcp", "127.0.0.1:"+port)
 	if err != nil {
 		return err
 	}
+	self.ctx, self.cancel = context.WithCancel(context.Background())
 	self.bufSize = 1024
 	self.ch = make(chan []byte, self.bufSize)
 	self.progress = pCallback
-	go self.listenRoutine(listen)
+	self.wg.Add(1)
+	go self.listenRoutine(self.ctx)
 	return nil
 }
 
@@ -29,26 +37,41 @@ func (self *ServerInterface) SetBufSize(size int) {
 	self.bufSize = size
 }
 
-func (self *ServerInterface) listenRoutine(listen net.Listener) {
+func (self *ServerInterface) listenRoutine(ctx context.Context) {
 	for {
-		conn, err := listen.Accept()
+		conn, err := self.listen.Accept()
 		if err != nil {
 			fmt.Println(err)
-			continue
+			break
 		}
-		go self.recRoutine(conn)
+		self.wg.Add(1)
+		go self.recRoutine(conn, ctx)
 	}
+	self.wg.Done()
 }
 
-func (self *ServerInterface) recRoutine(conn net.Conn) {
+func (self *ServerInterface) recRoutine(conn net.Conn, ctx context.Context) {
 	bytes := make([]byte, self.bufSize)
 	for {
 		n, err := conn.Read(bytes)
 		if err != nil {
 			continue
 		}
+
 		buf := bytes[:n]
-		self.ch <- buf
+		select {
+		case self.ch <- buf:
+		case <-ctx.Done():
+			goto end
+		}
 		self.progress(conn, self.ch)
 	}
+end:
+	self.wg.Done()
+}
+
+func (self *ServerInterface) Stop() {
+	self.listen.Close()
+	self.cancel()
+	self.wg.Wait()
 }
